70a4a6256686f079fcc5dbe258f0b970e5d61f8c
sudo-daemon <76221621+sudo-daemon@users.noreply.github.com>
2021-02-09 23:17:28 +0530

    Add more documentation

--- img.py
+++ img.py
@@ -1,8 +1,9 @@
+# Importing required modules
 from tkinter import *
 from tkinter import filedialog
 import os
 
-
+# Defining a variable used later
 success_token = False
 
 # Finding the required ISO file
@@ -27,15 +28,7 @@ def find_drive():
     finderDrive = Button(root, text="Submit", command=lambda: collect_drive_input())
     finderDrive.pack()
 
-
-# Setting the text input to a variable
-def collect_drive_input():
-    global verify_drive, one_drive
-    one_drive = drive_rough_location.get()
-    verify_drive = one_drive.startswith("/dev/")
-    setting_drive_path()
-
-
+# Ensuring that the drive path matches the '/dev/' format, to make sure the output location is a drive, not another directory
 def setting_drive_path():
     global drive_location, verify_token
     if verify_drive == True:
@@ -46,10 +39,17 @@ def setting_drive_path():
         verify_token = False
         error_message = Label(root, text="Sorry. You didn't enter a valid drive path. Try again. ")
         error_message.pack()
-
+    
+    
+# Setting the text input to a variable
+def collect_drive_input():
+    global verify_drive, one_drive
+    one_drive = drive_rough_location.get()
+    verify_drive = one_drive.startswith("/dev/")
+    setting_drive_path()
+    
 
 # Runs the 'dd' command with the correct input and output locations
-
 def warning():
     warning_message = Label(root, text="Warning! You cannot cancel the operation or remove the drive.")
     warning_message.pack()
@@ -59,8 +59,9 @@ def warning():
     finderDrive.destroy()
     wait_message_during_dd = Label(root, text="Burning ISO. Please wait...")
     wait_message_during_dd.pack()
-    print("Hello world")
+    # print("Hello world")
 
+# Executing the dd command with the correct parameters
 def dd():
     warning()
     os.system('dd if=' + str(iso_location) + " of=" + str(drive_location) + ">> ./.success_verification.txt ")
@@ -68,6 +69,7 @@ def dd():
     #os.system('echo Good luck! >> ./.success_verification.txt')
     success_message_file = open('./.success_verification.txt', 'r')
     success_message = success_message_file.read()
+    # The ultimate goal is to show this success message on the GUI by creating another 'Label' element containing the output of the dd command
     print(success_message)
 
 # Creating actual window
